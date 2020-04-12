7b547d64c0fac2b364c2123656fcc7cea86a1fc1
Smit Mehta <mehta.smit92@gmail.com>
2020-04-12 01:44:13 -0400

    Create api_call_v2.py

---
+++ data-ingestion/api_call_v2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+##############################################################################################################
+### Authors: Smit Mehta
+### Python:  Version 3.7.3
+### Details: script to download citibike real-time data
+### Notes:   Check documentation here: http://gbfs.citibikenyc.com/gbfs/gbfs.json
+###                                    https://github.com/NABSA/gbfs/blob/master/gbfs.md
+###          
+##############################################################################################################
+
