df103abd9448157fcf32b1f5c7dc64018f057f26
Smit Mehta <mehta.smit92@gmail.com>
2020-04-12 23:58:26 -0400

    Update api_call_v2.py

--- data-ingestion/api_call_v2.py
+++ data-ingestion/api_call_v2.py
@@ -14,6 +14,14 @@ import requests
 import time
 
 
+def getdata(url):
+    data = requests.get(url).json()
+    return data
+
+
+
 station_info_url = 'https://gbfs.citibikenyc.com/gbfs/en/station_information.json'
 station_status_url = 'https://gbfs.citibikenyc.com/gbfs/en/station_status.json'
-data = requests.get(url).json()
+
+
+
